532dc96e67fa1e096ec014aab9ec82969723bc5a
robert <robert@zklink.org>
2024-05-07 11:59:05 +0800

    fixd symbiosis fetch event error.

--- src/repositories/transfer.repository.ts
+++ src/repositories/transfer.repository.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { Transfer, TransferType } from "../entities";
-import { UnitOfWork } from "../unitOfWork";
+import { ExplorerUnitOfWork as UnitOfWork } from "../unitOfWork";
 import { BaseRepository } from "./base.repository";
 import { LessThan } from "typeorm";
 
@@ -86,7 +86,16 @@ export class TransferRepository extends BaseRepository<Transfer> {
     const usdtUsdcAddressesBuf = usdtUsdcAddresses.map((item) => {
       return Buffer.from(item.substring(2), "hex");
     });
-    const query = `SELECT * FROM "transfers" WHERE "blockNumber" > $1 AND "from" = ANY($2) AND (("tokenAddress" = ANY($3) AND cast(amount as numeric) >= $4) or ("tokenAddress" = ANY($5) AND cast(amount as numeric) >= $6)) order by "blockNumber" asc, number asc;`;
+    const query = `
+    SELECT * FROM "transfers"
+    WHERE "blockNumber" > $1 AND "from" = ANY ($2) AND
+    (
+        ("tokenAddress" = ANY($3) AND cast(amount AS numeric) >= $4)
+        OR
+        ("tokenAddress" = ANY($5) AND cast(amount AS numeric) >= $6)
+    )
+    ORDER BY "blockNumber" ASC, number ASC;
+    `;
     const results = await transactionManager.query(query, [
       fromBlockNumber,
       bridgeAddressesBuf,
@@ -94,6 +103,79 @@ export class TransferRepository extends BaseRepository<Transfer> {
       ethAmount,
       usdtUsdcAddressesBuf,
       usdtUsdcAmount,
+    ]);
+    return results.map((row: any) => {
+      row.to = "0x" + row.to.toString("hex");
+      row.from = "0x" + row.from.toString("hex");
+      row.tokenAddress = "0x" + row.tokenAddress.toString("hex");
+      return row;
+    });
+  }
+
+  public async getSymbiosisLatestQulifyTransfers(
+    fromBlockNumber: number,
+    bridgeAddresses: string[],
+    ethAddresses: string[],
+    ethAmount: bigint,
+    usdtUsdcAddresses: string[],
+    usdtUsdcAmount: bigint,
+    notEqualToAddress: string[],
+    baseBridgeAddress: string
+  ): Promise<Transfer[]> {
+    const transactionManager = this.unitOfWork.getTransactionManager();
+    const bridgeAddressesBuf = bridgeAddresses.map((item) => {
+      return Buffer.from(item.substring(2), "hex");
+    });
+    const ethAddressesBuf = ethAddresses.map((item) => {
+      return Buffer.from(item.substring(2), "hex");
+    });
+    const usdtUsdcAddressesBuf = usdtUsdcAddresses.map((item) => {
+      return Buffer.from(item.substring(2), "hex");
+    });
+    const notEqualToAddressBuf = notEqualToAddress.map((item) => {
+      return Buffer.from(item.substring(2), "hex");
+    });
+    const baseBridgeAddressBuf = Buffer.from(baseBridgeAddress.substring(2), "hex");
+    const topic0 = Buffer.from(
+      "0xaeef64b7687b985665b6620c7fa271b6f051a3fbe2bfc366fb9c964602eb6d26".substring(2),
+      "hex"
+    );
+    const eventQuery = `
+        SELECT "transactionHash" FROM logs
+        WHERE "blockNumber" > $1
+        AND address = $2
+        AND topics[1] = $3;
+    `;
+    const thashResults = await transactionManager.query(eventQuery, [fromBlockNumber, baseBridgeAddressBuf, topic0]);
+    const transactionHashs: Buffer[] = thashResults.map((row: any) => {
+      return row.transactionHash;
+    });
+    if (transactionHashs.length == 0) {
+      return [];
+    }
+    const query = `
+    SELECT * FROM "transfers"
+    WHERE "blockNumber" > $1
+    AND "from" = ANY($2)
+    AND
+        (
+            ("tokenAddress" = ANY($3) AND cast(amount AS numeric) >= $4)
+            OR
+            ("tokenAddress" = ANY($5) AND cast(amount AS numeric) >= $6)
+        )
+    AND "to" <> ALL($7)
+    AND "transactionHash" = ANY($8)
+    ORDER BY "blockNumber" ASC, number ASC;
+    `;
+    const results = await transactionManager.query(query, [
+      fromBlockNumber,
+      bridgeAddressesBuf,
+      ethAddressesBuf,
+      ethAmount,
+      usdtUsdcAddressesBuf,
+      usdtUsdcAmount,
+      notEqualToAddressBuf,
+      transactionHashs,
     ]);
     return results.map((row: any) => {
       row.to = "0x" + row.to.toString("hex");
