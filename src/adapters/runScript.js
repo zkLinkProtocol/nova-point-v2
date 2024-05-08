5d204895cdf12e0b205904a431ada64ee2ff0c86
xsteadybcgo <xw4585@gmail.com>
2024-05-09 00:32:22 +0800

    fix: update runScript

--- src/adapters/runScript.js
+++ src/adapters/runScript.js
@@ -1,27 +1,25 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 const fs = require("fs");
 const path = require("path");
 
-const csv = require("csv-parser");
 const { write } = require("fast-csv");
 
 // Get the folder name from command line arguments
 const folderName = process.argv[2];
-const blockNumber = process.argv[3];
-const blockTimestamp = process.argv[4];
+const lastBlockNumber = process.argv[3];
+const curBlockNumber = process.argv[4];
 
 if (!folderName) {
   console.error("Folder name not provided. Please provide the folder name as an argument.");
   process.exit(1);
 }
 
-if (!blockNumber) {
-  console.error("BlockNumber not provided. Please provide the blockNumber as an argument.");
+if (!lastBlockNumber) {
+  console.error("lastBlockNumber not provided. Please provide the lastBlockNumber as an argument.");
   process.exit(1);
 }
 
-if (!blockTimestamp) {
-  console.error("BlockTimestamp not provided. Please provide the blockTimestamp as an argument.");
+if (!curBlockNumber) {
+  console.error("curBlockNumber not provided. Please provide the curBlockNumber as an argument.");
   process.exit(1);
 }
 
@@ -42,66 +40,184 @@ if (!fs.existsSync(indexPath)) {
 }
 
 // Import the funct function from the provided folder
-const { getUserBalanceByBlock } = require(indexPath);
+const { getUserTVolByBlock, getUserTVLByBlock, getUserTxNumByBlock } = require(indexPath);
 
-let rowSet = [];
-getUserBalanceByBlock(Number(blockNumber), Number(blockTimestamp)).then((result) => {
-  const allCsvRows = [];
-  try {
-    // check : item of result must be an object with keys: address, poolAddress, tokenAddress, blockNumber, balance
-    let duplicateKeys = 0;
-    for (const item of result) {
-      if (
-        !item.userAddress ||
-        !item.poolAddress ||
-        !item.tokenAddress ||
-        undefined === item.balance ||
-        typeof item.userAddress !== "string" ||
-        typeof item.poolAddress !== "string" ||
-        typeof item.tokenAddress !== "string" ||
-        typeof item.balance !== "bigint"
-      ) {
-        console.error("Invalid item, key:", key, ", item:", item);
-        console.error("Exiting the process due to invalid item, please fix the issue and try again.");
-        process.exit(1);
+if (getUserTVolByBlock) {
+  getUserTVolByBlock(Number(lastBlockNumber), Number(curBlockNumber)).then((result) => {
+    const allCsvRows = [];
+    const keyMap = new Map();
+    try {
+      // check : item of result must be an object with keys: address, poolAddress, tokenAddress, blockNumber, balance
+      for (const item of result) {
+        if (
+          !item.userAddress ||
+          !item.poolAddress ||
+          !item.tokenAddress ||
+          !item.decimals ||
+          !item.price ||
+          !item.quantity ||
+          !item.txHash
+        ) {
+          console.error("Invalid item, key:", tem.txHash, ", item:", item);
+          console.error(
+            "Exiting the process due to invalid item in getUserTVolByBlock, please fix the issue and try again."
+          );
+          process.exit(1);
+        }
+
+        const key = tem.txHash;
+        if (keyMap.get(key)) {
+          console.error("Duplicate key: ", key);
+          console.error("Exiting the process due to duplicate key, please fix the issue and try again.");
+          process.exit(1);
+        } else {
+          keyMap.set(key, true);
+        }
+      }
+      if (duplicateKeys > 0) {
       }
 
-      const kkey = `${item.address}_${item.poolAddress}_${item.tokenAddress}`;
-      if (rowSet[kkey]) {
-        console.error("Duplicate key: ", kkey);
-        duplicateKeys++;
-      } else {
-        rowSet[kkey] = item;
-      }
-    }
-    if(duplicateKeys > 0){
-      console.error("Exiting the process due to duplicate key, please fix the issue and try again.");
-      process.exit(1);
-    }
-
-    const resultTmp = result.map((item) => {
+      const resultTmp = result.map((item) => {
         return {
-            userAddress: item.userAddress,
-            poolAddress: item.poolAddress,
-            tokenAddress: item.tokenAddress,
-            blockNumber: blockNumber,
-            balance: item.balance
+          userAddress: item.userAddress,
+          poolAddress: item.poolAddress,
+          tokenAddress: item.tokenAddress,
+          decimals: item.decimals,
+          price: item.price,
+          quantity: item.quantity,
+          txHash: item.txHash,
+          blockNumber: curBlockNumber,
         };
-    });
-
-    // Accumulate CSV rows for all blocks
-    allCsvRows.push(...resultTmp);
-
-    // Write to file when batch size is reached or at the end of loop
-    const ws = fs.createWriteStream(`${folderName}/data/output.${blockNumber}.csv`, { flags: "w" });
-    write(allCsvRows, { headers: true })
-      .pipe(ws)
-      .on("finish", () => {
-        console.log(`CSV file has been written.`);
       });
-    // Clear the accumulated CSV rows
-    allCsvRows.length = 0;
-  } catch (error) {
-    console.error(`An error occurred for block ${blockNumber}:`, error);
-  }
-});
+
+      // Accumulate CSV rows for all blocks
+      allCsvRows.push(...resultTmp);
+
+      // Write to file when batch size is reached or at the end of loop
+      const ws = fs.createWriteStream(`${folderName}/data/output.vol.${blockNumber}.csv`, { flags: "w" });
+      write(allCsvRows, { headers: true })
+        .pipe(ws)
+        .on("finish", () => {
+          console.log(`CSV file has been written.`);
+        });
+      // Clear the accumulated CSV rows
+      allCsvRows.length = 0;
+    } catch (error) {
+      console.error(`An error occurred for block ${blockNumber}:`, error);
+    }
+  });
+}
+
+if (getUserTVLByBlock) {
+  getUserTVLByBlock(Number(curBlockNumber)).then((result) => {
+    const allCsvRows = [];
+    const keyMap = new Map();
+    try {
+      // check : item of result must be an object with keys: address, poolAddress, tokenAddress, blockNumber, balance
+      for (const item of result) {
+        const key = item.userAddress;
+
+        if (!item.userAddress || !item.poolAddress || !item.tokenAddress || !item.balance) {
+          console.error("Invalid item, key:", item.userAddress, ", item:", item);
+          console.error("Exiting the process due to invalid item, please fix the issue and try again.");
+          process.exit(1);
+        }
+
+        if (keyMap.get(key)) {
+          console.error("Duplicate key: ", key);
+          console.error("Exiting the process due to duplicate key, please fix the issue and try again.");
+          process.exit(1);
+        } else {
+          keyMap.set(key, true);
+        }
+      }
+
+      const resultTmp = result.map((item) => {
+        return {
+          userAddress: item.userAddress,
+          poolAddress: item.poolAddress,
+          tokenAddress: item.tokenAddress,
+          blockNumber: blockNumber,
+          balance: item.balance,
+        };
+      });
+
+      // Accumulate CSV rows for all blocks
+      allCsvRows.push(...resultTmp);
+
+      // Write to file when batch size is reached or at the end of loop
+      const ws = fs.createWriteStream(`${folderName}/data/output.tvl.${blockNumber}.csv`, { flags: "w" });
+      write(allCsvRows, { headers: true })
+        .pipe(ws)
+        .on("finish", () => {
+          console.log(`CSV file has been written.`);
+        });
+      // Clear the accumulated CSV rows
+      allCsvRows.length = 0;
+    } catch (error) {
+      console.error(`An error occurred for block ${blockNumber}:`, error);
+    }
+  });
+}
+
+if (getUserTxNumByBlock) {
+  getUserTxNumByBlock(Number(lastBlockNumber), Number(curBlockNumber)).then((result) => {
+    const allCsvRows = [];
+    const keyMap = new Map();
+    try {
+      // check : item of result must be an object with keys: address, poolAddress, tokenAddress, blockNumber, balance
+      for (const item of result) {
+        const key = item.txHash;
+        if (
+          !item.userAddress ||
+          !item.poolAddress ||
+          !item.tokenAddress ||
+          !item.decimals ||
+          !item.price ||
+          !item.quantity ||
+          !item.txHash
+        ) {
+          console.error("Invalid item, key:", key, ", item:", item);
+          console.error("Exiting the process due to invalid item, please fix the issue and try again.");
+          process.exit(1);
+        }
+
+        if (keyMap.get(key)) {
+          console.error("Duplicate key: ", key);
+          console.error("Exiting the process due to duplicate key, please fix the issue and try again.");
+          process.exit(1);
+        } else {
+          keyMap.set(key, true);
+        }
+      }
+
+      const resultTmp = result.map((item) => {
+        return {
+          userAddress: item.userAddress,
+          poolAddress: item.poolAddress,
+          tokenAddress: item.tokenAddress,
+          decimals: item.decimals,
+          price: item.price,
+          quantity: item.quantity,
+          txHash: item.txHash,
+          blockNumber: curBlockNumber,
+        };
+      });
+
+      // Accumulate CSV rows for all blocks
+      allCsvRows.push(...resultTmp);
+
+      // Write to file when batch size is reached or at the end of loop
+      const ws = fs.createWriteStream(`${folderName}/data/output.vol.${blockNumber}.csv`, { flags: "w" });
+      write(allCsvRows, { headers: true })
+        .pipe(ws)
+        .on("finish", () => {
+          console.log(`CSV file has been written.`);
+        });
+      // Clear the accumulated CSV rows
+      allCsvRows.length = 0;
+    } catch (error) {
+      console.error(`An error occurred for block ${blockNumber}:`, error);
+    }
+  });
+}
