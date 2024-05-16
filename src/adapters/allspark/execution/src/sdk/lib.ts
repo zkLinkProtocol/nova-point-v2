6b6d351d7c02908ffed6df96ccae12c0b2941d23
MaxwellBlock <87404816+MaxwellBlock@users.noreply.github.com>
2024-05-17 01:58:10 +0800

    Submit Allspark Adapter
    Submit Allspark Adapter

---
+++ src/adapters/allspark/execution/src/sdk/lib.ts
@@ -0,0 +1,27 @@
+import { UserTxData } from "./types";
+
+export async function getUserTXAtBlock(startBlock: number, endBlock: number) {
+  const response = await fetch('https://api.allspark.finance/zklink-mantissa-api/getNovaPoint', {
+      method: 'POST',
+      body: JSON.stringify({ startBlock: startBlock, endBlock: endBlock }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+  const data = await response.json();
+  const userTxData: UserTxData[] = data.data.map((item: any) => {
+    return {
+      timestamp: item.timestamp,
+      userAddress: item.userAddress,
+      contractAddress: item.contractAddress,
+      tokenAddress: item.tokenAddress,
+      decimals: item.decimals,
+      price: item.price,
+      quantity: BigInt(item.quantity),
+      txHash: item.txHash,
+      nonce: item.nonce,
+      blockNumber: item.blockNumber,
+    };
+  });
+
+  return userTxData;
+}
