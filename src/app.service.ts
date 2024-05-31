7024b533815669fc6df002ad57f06bb3b741ca58
xsteadybcgo <xw4585@gmail.com>
2024-05-31 17:21:20 +0800

    fix: novaswap adapter issue

--- src/app.service.ts
+++ src/app.service.ts
@@ -32,7 +32,7 @@ export class AppService implements OnModuleInit, OnModuleDestroy {
     // await this.adapterService.loadLastBlockNumber(1476336, 1376336);
     // second params is utc+8
     // await this.tvlPointService.handleHoldPoint(1395273, new Date(1715159940 * 1000).toISOString());
-    // this.compensatePoints()
+    this.compensatePoints()
 
 
     this.startWorkers();
@@ -61,10 +61,16 @@ export class AppService implements OnModuleInit, OnModuleDestroy {
   }
 
   private async compensatePoints() {
-
-    await this.adapterService.compensatePointsData('interport', 2051369, 2037300);
-
-    await this.tvlPointService.handleHoldPoint(2051369, new Date("2024-05-25 09:21:55Z").toISOString());
+    await this.adapterService.compensatePointsData('novaswap', 2228865, 2215814);
+    await this.tvlPointService.handleHoldPoint(2051369, new Date("2024-05-29 17:22:55Z").toISOString());
+    await this.adapterService.compensatePointsData('novaswap', 2242907, 2228865);
+    await this.tvlPointService.handleHoldPoint(2051369, new Date("2024-05-30 01:22:55Z").toISOString());
+    await this.adapterService.compensatePointsData('novaswap', 2256932, 2242907);
+    await this.tvlPointService.handleHoldPoint(2051369, new Date("2024-05-30 09:22:55Z").toISOString());
+    await this.adapterService.compensatePointsData('novaswap', 2270939, 2256932);
+    await this.tvlPointService.handleHoldPoint(2051369, new Date("2024-05-30 17:22:55Z").toISOString());
+    await this.adapterService.compensatePointsData('novaswap', 2284939, 2270939);
+    await this.tvlPointService.handleHoldPoint(2051369, new Date("2024-05-31 01:22:55Z").toISOString());
   }
 
 }
