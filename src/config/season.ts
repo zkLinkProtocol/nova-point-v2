cefc39d4987c6bacbd1b793cfb33904ef13b87bf
robert <robert@zklink.org>
2024-07-02 11:29:55 +0800

    change season point from blocknumber.

--- src/config/season.ts
+++ src/config/season.ts
@@ -1,12 +1,16 @@
 export default [
   {
     season: 1,
-    startTime: "2023-01-01 00:00:00",
+    startTime: "2021-01-01 00:00:00",
     endTime: "2024-05-30 00:00:00",
+    startBlockNumber: 0,
+    endBlockNumber: 2226304,
   },
   {
     season: 2,
     startTime: "2024-05-30 00:00:00",
     endTime: "2024-07-14 00:00:00",
+    startBlockNumber: 2226304,
+    endBlockNumber: 99999999,
   },
 ];
