4800f39b5950c4b974c91d90bb4a0b91fe91e96c
robert <robert@zklink.org>
2024-07-09 16:09:29 +0800

    update index name .

--- src/migrations/1720455754693-CalculateAndUpdateReferralPoints.ts
+++ src/migrations/1720455754693-CalculateAndUpdateReferralPoints.ts
@@ -6,19 +6,19 @@ export class CalculateAndUpdateReferralPoints1720455754693 implements MigrationI
     await queryRunner.query(`ALTER TABLE "referralPoints" ADD COLUMN "season" numeric DEFAULT 0;`);
     await queryRunner.query(`CREATE TABLE "referralPoints_backup" AS SELECT * FROM "referralPoints";`);
     await queryRunner.query(`TRUNCATE TABLE "referralPoints";`);
-    await queryRunner.query(`DROP INDEX "idx_referralPoints_1";`);
+    await queryRunner.query(`DROP INDEX "PK_1893dd931640dcef5cc364e2727";`);
     await queryRunner.query(
-      `CREATE UNIQUE INDEX "idx_referralPoints_1" ON "referralPoints" ("address", "pairAddress", "season");`
+      `CREATE UNIQUE INDEX "PK_1893dd931640dcef5cc364e2727" ON "referralPoints" ("address", "pairAddress", "season");`
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP INDEX "idx_referralPoints_1";`);
+    await queryRunner.query(`DROP INDEX "PK_1893dd931640dcef5cc364e2727";`);
     await queryRunner.query(`DROP TABLE "referralPoints";`);
     await queryRunner.query(`ALTER TABLE "referralPoints_backup" RENAME TO "referralPoints";`);
     await queryRunner.query(`ALTER TABLE "referralPoints" DROP COLUMN "season";`);
     await queryRunner.query(
-      `CREATE UNIQUE INDEX "idx_referralPoints_1" ON "referralPoints" ("address", "pairAddress");`
+      `CREATE UNIQUE INDEX "PK_1893dd931640dcef5cc364e2727" ON "referralPoints" ("address", "pairAddress");`
     );
     await queryRunner.query(`ALTER TABLE "blockReferralPoints_delete" RENAME TO "blockReferralPoints";`);
   }
