fd0d16a0f09c7913adb23c19210ce45f6426df5d
formysister <enemymaycry2007@gmail.com>
2024-08-11 05:37:18 +0900

    update: zns-points subgraph added

--- src/adapters/zns/execution/src/index.ts
+++ src/adapters/zns/execution/src/index.ts
@@ -1,8 +1,8 @@
 import { UserTxData } from './sdk/types';
 import { queryUserTxData } from "./sdk/lib";
 
-export const getUserTransactionData = async (): Promise<Array<UserTxData>> => {
-    const userTransactionData = await queryUserTxData(0, 38321929)
+export const getUserTransactionData = async (startBlock: number, endBlock: number): Promise<Array<UserTxData>> => {
+    const userTransactionData = await queryUserTxData(startBlock, endBlock)
 
     return userTransactionData
 };
