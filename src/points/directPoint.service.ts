504fd9c3dc50f95f88dfc7c191b4fb2547dc7095
robert <robert@zklink.org>
2024-08-23 01:12:51 +0800

    fix await.

--- src/points/directPoint.service.ts
+++ src/points/directPoint.service.ts
@@ -320,20 +320,24 @@ export class DirectPointService extends Worker {
       }
       item.stakePoint = Number(item.stakePoint) + Number(holdPoint);
     }
-    await this.unitOfWork.useTransaction(async () => {
-      try {
-        this.logger.log(`Start insert directHolding point into db for block: ${blockNumber}`);
-        await this.blockAddressPointRepository.addManyIgnoreConflicts(blockAddressPoints);
-        this.logger.log(
-          `Finish directHolding blockAddressPoints for block: ${blockNumber}, length: ${blockAddressPoints.length}`
-        );
-        await this.pointsRepository.addManyOrUpdate(newAddressPoints, ["stakePoint"], ["address"]);
-        this.logger.log(
-          `Finish directHolding newAddressPoints for block: ${blockNumber}, length: ${newAddressPoints.length}`
-        );
-      } catch (error) {
-        this.logger.error(`Failed to update hold points for block ${blockNumber}: ${error.stack}`);
-      }
+    await new Promise<void>((resolve, reject) => {
+      this.unitOfWork.useTransaction(async () => {
+        try {
+          this.logger.log(`Start insert directHolding point into db for block: ${blockNumber}`);
+          await this.blockAddressPointRepository.addManyIgnoreConflicts(blockAddressPoints);
+          this.logger.log(
+            `Finish directHolding blockAddressPoints for block: ${blockNumber}, length: ${blockAddressPoints.length}`
+          );
+          await this.pointsRepository.addManyOrUpdate(newAddressPoints, ["stakePoint"], ["address"]);
+          this.logger.log(
+            `Finish directHolding newAddressPoints for block: ${blockNumber}, length: ${newAddressPoints.length}`
+          );
+          resolve();
+        } catch (error) {
+          this.logger.error(`Failed to update hold points for block ${blockNumber}: ${error.stack}`);
+          reject(error);
+        }
+      });
     });
   }
 
