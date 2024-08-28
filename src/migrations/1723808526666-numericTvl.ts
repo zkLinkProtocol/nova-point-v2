3488a3f61d694eded5550d06b70ec471a392ad54
robin <robin@zklink.org>
2024-08-28 16:59:22 +0800

    name

--- src/migrations/1723808526666-numericTvl.ts
+++ src/migrations/1723808526666-numericTvl.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class Migrations1723808526666 implements MigrationInterface {
-  name = "numericTvl";
+  name = "numericTvl1723808526666";
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
