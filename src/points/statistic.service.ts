4521f9ab8a2b067906e9a5fc05984f6fa1bfe187
robin <robin@zklink.org>
2024-08-28 13:30:37 +0800

    time

--- src/points/statistic.service.ts
+++ src/points/statistic.service.ts
@@ -154,7 +154,7 @@ export class StatisticService {
   }
 
   // Historical data, run only once
-  @Cron("0 30 5 * * *")
+  @Cron("0 40 5 * * *")
   public async statisticHistoryTvl() {
     this.logger.log("begin statistics statisticHistoryTvl");
     const result: { min: string; max: string }[] = await this.balanceOfLpRepository.query(
