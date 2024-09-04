c2be9d67f131f63f14d59ad75a564c8bc1a376be
robert <robert@zklink.org>
2024-09-04 17:49:51 +0800

    op memory.

--- src/points/directPoint.service.ts
+++ src/points/directPoint.service.ts
@@ -157,24 +157,29 @@ export class DirectPointService extends Worker {
       // get all first deposit time
       const addressFirstDepositMap = await this.addressFirstDepositRepository.getFirstDepositMapForAddresses(addresses);
       for (const address of addresses) {
-        const addressTvl = addressTvlMap.get(address);
-        const addressMultiplier = this.getAddressMultiplier(address, blockTs);
-
-        // get the last multiplier before the block timestamp
-        // const addressFirstDepositTime = addressFirstDepositMap.get(address.toLowerCase());
-        //let groupBooster = new BigNumber(1);
-        const loyaltyBooster = 1; //this.getLoyaltyBooster(blockTs, addressFirstDepositTime?.getTime());
-        // NOVA Point = sum_all tokens in activity list (Early_Bird_Multiplier * Token Multiplier * Address Multiplier * Token Amount * Token Price * (1 + Group Booster + Growth Booster) * Loyalty Booster / ETH_Price )
-        const newHoldPoint = addressTvl.holdBasePoint
+        let addressTvl = addressTvlMap.get(address);
+        let addressMultiplier = this.getAddressMultiplier(address, blockTs);
+        // let addressFirstDepositTime = addressFirstDepositMap.get(address.toLowerCase());
+        //let groupBooster = 1;
+        let loyaltyBooster = 1; //this.getLoyaltyBooster(blockTs, addressFirstDepositTime?.getTime());
+        let newHoldPoint = addressTvl.holdBasePoint
           .multipliedBy(earlyBirdMultiplier)
           // .multipliedBy(groupBooster)
           .multipliedBy(addressMultiplier)
           .multipliedBy(loyaltyBooster);
+
         // addressHoldPoints.push({
         //   address,
         //   holdPoint: newHoldPoint.toNumber(),
         //   blockNumber: currentStatisticalBlock.number,
         // });
+
+        addressTvl = null;
+        addressMultiplier = null;
+        // addressFirstDepositMap = null;
+        // groupBooster = null;
+        loyaltyBooster = null;
+        newHoldPoint = null;
       }
       this.logger.log(`Finishloop address, blockNumber:${currentStatisticalBlock.number}`);
       // await this.updateHoldPoint(addressHoldPoints, currentStatisticalBlock.number);
