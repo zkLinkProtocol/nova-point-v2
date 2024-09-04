b603941c29883bb9585a459992741562fe005764
robert <robert@zklink.org>
2024-09-04 15:25:44 +0800

    op memory

--- src/points/directPoint.service.ts
+++ src/points/directPoint.service.ts
@@ -163,18 +163,18 @@ export class DirectPointService extends Worker {
         // get the last multiplier before the block timestamp
         // const addressFirstDepositTime = addressFirstDepositMap.get(address.toLowerCase());
         let groupBooster = new BigNumber(1);
-        // const loyaltyBooster = 1; //this.getLoyaltyBooster(blockTs, addressFirstDepositTime?.getTime());
+        const loyaltyBooster = 1; //this.getLoyaltyBooster(blockTs, addressFirstDepositTime?.getTime());
         // NOVA Point = sum_all tokens in activity list (Early_Bird_Multiplier * Token Multiplier * Address Multiplier * Token Amount * Token Price * (1 + Group Booster + Growth Booster) * Loyalty Booster / ETH_Price )
         const newHoldPoint = addressTvl.holdBasePoint
           .multipliedBy(earlyBirdMultiplier)
           .multipliedBy(groupBooster)
-          .multipliedBy(addressMultiplier);
-        // .multipliedBy(loyaltyBooster);
-        addressHoldPoints.push({
-          address,
-          holdPoint: newHoldPoint.toNumber(),
-          blockNumber: currentStatisticalBlock.number,
-        });
+          .multipliedBy(addressMultiplier)
+          .multipliedBy(loyaltyBooster);
+        // addressHoldPoints.push({
+        //   address,
+        //   holdPoint: newHoldPoint.toNumber(),
+        //   blockNumber: currentStatisticalBlock.number,
+        // });
       }
       this.logger.log(`Finishloop address, blockNumber:${currentStatisticalBlock.number}`);
       // await this.updateHoldPoint(addressHoldPoints, currentStatisticalBlock.number);
