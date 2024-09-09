666ea34d02cce22c466ceea52b6facb0fbbce18a
robert <robert@zklink.org>
2024-09-09 21:13:25 +0800

    stop direct points.

--- src/app.service.ts
+++ src/app.service.ts
@@ -70,7 +70,7 @@ export class AppService implements OnModuleInit, OnModuleDestroy {
       this.calTvlPointService.start(),
       this.calTxPointService.start(),
       this.seasonTotalPointService.start(),
-      this.directPointService.start(),
+      // this.directPointService.start(),
     ]);
   }
 
