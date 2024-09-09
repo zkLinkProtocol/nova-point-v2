e2b40a2cacd68562682f9019ab96e04ce057fdfb
robert <robert@zklink.org>
2024-09-09 10:01:11 +0800

    op mem

--- src/migrations/1723947331873-UpdateDirectPointProcessingStatus.ts
+++ src/migrations/1723947331873-UpdateDirectPointProcessingStatus.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class UpdateDirectPointProcessingStatus1723947331872 implements MigrationInterface {
+export class UpdateDirectPointProcessingStatus1723947331873 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
             UPDATE "directHoldProcessingStatus"
