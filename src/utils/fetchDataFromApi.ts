f989d10b4ff4587aaf0ce7c9368cbb0a26318c83
xsteadybcgo <xw4585@gmail.com>
2024-10-15 15:33:02 +0800

    fix: fetch data

--- src/utils/fetchDataFromApi.ts
+++ src/utils/fetchDataFromApi.ts
@@ -33,12 +33,8 @@ export const fetchAPIData = async <T>(url: string): Promise<any> => {
     if (retryCount >= maxRetries) {
         console.error("Maximum retry limit reached");
     }
-    console.log(data)
-    return [
-        { address: '0x1234567890abcdef1234567890abcdef12345678', amount: 1 },
-        { address: '0xabcdefabcdefabcdefabcdefabcdefabcdefabcd', amount: 0.5 },
-        { address: '0x3E58b7Ccd980d24Bd04b44Dca656a2e2941b2227', amount: 0.5 }]
-    // return data;
+
+    return data;
 };
 
 
